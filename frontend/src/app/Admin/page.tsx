42e17868d1f54856bac2e91f6bc858d6c447fac7
MD ASADUJZAMAN <asadujzaman.cst@gmail.com>
2025-11-03 22:18:46 +0600

    Update API endpoint for login request

--- frontend/src/app/Admin/page.tsx
+++ frontend/src/app/Admin/page.tsx
@@ -14,7 +14,7 @@ export default function LoginPage() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/token/", {
+      const res = await fetch("https://institutemanagement3.onrender.com/api/token/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
